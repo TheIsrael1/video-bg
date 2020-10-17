b0f0373e997813b36cdfc584373ea86729dc5090
Ehindero Israel <iehindero419@stu.ui.edu.ng>
2020-10-16 21:36:18 -0700

    video background

---
+++ app.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector('.switch-btn');
+const video = document.querySelector(".video-c");
+
+btn.addEventListener('click', function () {
+
+ if (!btn.classList.contains('slide')) {
+  btn.classList.add("slide");
+  video.pause();
+ } else {
+  btn.classList.remove("slide");
+  video.play();
+ }
+
+});
+
